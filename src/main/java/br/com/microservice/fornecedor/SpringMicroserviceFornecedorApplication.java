ce418afb65fb7f6c5bf79fd8e472e0fbf4ac2243
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-01-06 15:20:50 -0300

    upload project

---
+++ src/main/java/br/com/microservice/fornecedor/SpringMicroserviceFornecedorApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservice.fornecedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceFornecedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceFornecedorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/microservice/fornecedor/controller/ProviderController.java
@@ -0,0 +1,10 @@
+package br.com.microservice.fornecedor.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/info")
+public class ProviderController {
+
+	
+	
+}
